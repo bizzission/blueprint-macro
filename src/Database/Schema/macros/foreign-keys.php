ac4a925c251c67c905cc7ebc7583e90eb9b60d8d
devel-pa <devel.pa@gmail.com>
2021-02-24 18:40:23 +0200

    Big int by default. Add onUpdate and onDelete on fks.

--- src/Database/Schema/macros/foreign-keys.php
+++ src/Database/Schema/macros/foreign-keys.php
@@ -6,41 +6,50 @@ use Illuminate\Support\Str;
 /*
  * Foreign Key
  */
-Blueprint::macro('addForeign', function($table, $options = []) {
-    $fk = (isset($options['fk']) && ! empty($options['fk'])) ?
+
+Blueprint::macro('addForeign', function ($table, $options = []) {
+    $fk = (isset($options['fk']) && !empty($options['fk'])) ?
         $options['fk'] : strtolower(Str::singular($table)) . '_id';
 
-    $reference = (isset($options['reference']) && ! empty($options['reference'])) ?
+    $reference = (isset($options['reference']) && !empty($options['reference'])) ?
         $options['reference'] : 'id';
 
-    if (isset($options['bigInteger']) && true == $options['bigInteger']) {
-        $schema = $this->unsignedBigInteger($fk)->index();
-    } else {
+    if (isset($options['bigInteger']) && false == $options['bigInteger']) {
         $schema = $this->unsignedInteger($fk)->index();
+    } else {
+        $schema = $this->unsignedBigInteger($fk)->index();
     }
 
     if (isset($options['nullable']) && true == $options['nullable']) {
         $schema->nullable();
     }
 
-    if (! isset($options['no_reference'])) {
+    if (!isset($options['no_reference'])) {
         $this->referenceOn($fk, $table, $reference);
+    }
+
+    if (isset($options['delete'])) {
+        $schema->onDelete($options['delete']);
+    }
+
+    if (isset($options['update'])) {
+        $schema->onUpdate($options['update']);
     }
 
     return $schema;
 });
 
- Blueprint::macro('addNullableForeign', function($table, $fk, $bigInteger = false) {
-     return $this->addForeign($table, ['nullable' => true, 'fk' => $fk, 'bigInteger' => $bigInteger])->comment('Nullable FK for ' . $table);
- });
+Blueprint::macro('addNullableForeign', function ($table, $fk, $bigInteger = true) {
+    return $this->addForeign($table, ['nullable' => true, 'fk' => $fk, 'bigInteger' => $bigInteger])->comment('Nullable FK for ' . $table);
+});
 
-Blueprint::macro('referenceOn', function($key, $table, $reference = 'id') {
+Blueprint::macro('referenceOn', function ($key, $table, $reference = 'id') {
     return $this->foreign($key)
         ->references($reference)
         ->on($table);
 });
 
-Blueprint::macro('belongsTo', function($table, $key = null, $bigInteger = false, $reference = 'id') {
+Blueprint::macro('belongsTo', function ($table, $key = null, $bigInteger = true, $reference = 'id') {
     if (is_null($key)) {
         $key = strtolower(Str::singular($table)) . '_id';
     }
@@ -48,7 +57,7 @@ Blueprint::macro('belongsTo', function($table, $key = null, $bigInteger = false,
     return $this->addForeign($table, ['fk' => $key, 'reference' => $reference, 'bigInteger' => $bigInteger])->comment('FK for ' . $table);
 });
 
-Blueprint::macro('nullableBelongsTo', function($table, $key = null, $bigInteger = false, $reference = 'id') {
+Blueprint::macro('nullableBelongsTo', function ($table, $key = null, $bigInteger = true, $reference = 'id') {
     if (is_null($key)) {
         $key = strtolower(Str::singular($table)) . '_id';
     }
